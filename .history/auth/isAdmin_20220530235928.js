e3ad3457f26aca13890e94e51e0422e36142b2ba
DangNguyen <huudang28420@gmail.com>
2022-05-31 11:27:17 +0700

    Update transfer

---
+++ .history/auth/isAdmin_20220530235928.js
@@ -0,0 +1,12 @@
+
+const bcrypt = require("bcrypt");
+
+module.exports = async function  (req, res, next) {
+    let {username, password} = req.body;
+    let checkPass = await bcrypt.compare(password, "$2b$10$fAH8neqtqI6X5pL5LE6wvunv9r3ZMAQxLFrnofHcITu8L7ax9NESe");
+    if(username==="admin" &&  checkPass == true){
+        return res.redirect("/admin")
+    }
+    next();
+  };
+  
